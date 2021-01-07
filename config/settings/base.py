8fc686c46adc9ef21f8712ce6a5b7ebd17037b90
tapia <tapiaw38@gmail.com>
2021-01-07 15:02:13 -0300

    add * origin

--- config/settings/base.py
+++ config/settings/base.py
@@ -36,11 +36,9 @@ AUTH_USER_MODEL = 'user.User'
 # Cors origins
 CORS_ALLOW_ALL_ORIGINS = True
 
-"""
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:8080',
+    '*',
 ]
-"""
 
 # Apps
 DJANGO_APPS = [
